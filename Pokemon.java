1da5551affe41e9b6c5321936dfb23ca3419300c
EarthSutiwat <60782337+EarthSutiwat@users.noreply.github.com>
2020-02-08 03:30:49 +0700

    Add new attribute/method  ,  Edit some method
    -New attribute
    * damage
    -New method
    * eat berry

--- Pokemon.java
+++ Pokemon.java
@@ -1,10 +1,12 @@
-
+  import java.util.Scanner;
    import java.util.Random;
    
    public class Pokemon {
     
     //attribute
 
+    Random rand = new Random(); 
+    Scanner scan = new Scanner (System.in); 
       private String species;
       private String name;
       private boolean checkChangeName = true;
@@ -13,31 +15,34 @@
       private int hp;
       private int atk;
       private int def;
+      private int damage;
       private int baseHp = 30;
       private int baseAtk = 56;
       private int baseDef = 35;
-      Random rand = new Random(); 
-      
+     
   
       //method
 
       public Pokemon ()
     {   
-
+        species = "Rattata";
         name = species;
         level =  rand.nextInt(9) + 2;
-        type = "normal";
+        type = "Normal";
         hp =  (int) ( (baseHp * 2) * (level/100.0)) + level +10;
         atk = (int) ( (baseAtk * 2) * (level/100.0)) + 5;
         def = (int) ( (baseDef * 2) * (level/100.0)) + 5 ;
+        damage = 0;
     }
       
       public void showStatus ()
       {
-        System.out.println(level);   
-        System.out.println(hp); 
-        System.out.println(atk);   
-        System.out.println(def);    
+        System.out.println("\nName: " +name);
+        System.out.println("Type: " +type);
+        System.out.println("Level: " +level);   
+        System.out.println("HP: " + (hp-damage) + " / " +hp ); 
+        System.out.println("ATK: " +atk);   
+        System.out.println("DEF: " +def);    
         
       }
 
@@ -45,11 +50,24 @@
        
         if (checkChangeName)
         {
-          System.out.println("Would you like to give " + name + " a nickname");
+          System.out.print("\nGive " + name + " a nickname: ");
+          name = scan.nextLine();
           checkChangeName = false;
         }
-        else System.out.print("You can't change name anymore!!!");
+        else System.out.println("\nYou can't change name anymore!!!");
       }
 
+      public void eatBerry(){
+        if (damage == 0)
+          System.out.println("\nNo!!! " + name + " is full.");
+        else {
+
+          System.out.println( name + " is eating berry.");
+
+            if(damage-10 < 0)
+              damage = 0;
+            else damage -= 10;
+        }
+      }
       
     }
