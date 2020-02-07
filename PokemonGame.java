914ded12ee7a619d223d8b79a8c41a57fef32112
EarthSutiwat <60782337+EarthSutiwat@users.noreply.github.com>
2020-02-08 03:32:41 +0700

    Add new method  /   Change variable's name
    New method
    - menu

--- PokemonGame.java
+++ PokemonGame.java
@@ -2,14 +2,48 @@ import java.util.Scanner;
 
 
 public class PokemonGame{
+     
+    
 
     public static void main(String [] args){
 
-        Pokemon Pichu= new Pokemon();
-        Pokemon Mankey = new Pokemon();
-
+        Pokemon myPoke = new Pokemon();
+        Pokemon wildPoke = new Pokemon();
         
-        Pichu.showStatus();
-        
+        menu(myPoke);
     }
+
+    private static void menu(Pokemon poke)
+    {    
+        Scanner scan = new Scanner (System.in); 
+        boolean endMenu = false ;   
+        
+        System.out.println("Check Pokemon's Status (PRESS 1)");
+        System.out.println("Change Pokemon's Name (PRESS 2)");
+        System.out.println("Give a berry. (PRESS 3)");
+        System.out.println("End MENU (Press 0)");
+        do {
+
+            System.out.print("\nSelect Menu: ");
+            int ans = scan.nextInt();
+            
+            switch (ans) {
+             case 1:
+             poke.showStatus();
+                break;
+             case 2:
+                poke.changeName();
+                break;
+             case 3:
+                poke.eatBerry();
+                break;
+             default:
+                endMenu = true;
+               }
+         }
+        while(endMenu == false);
+
+  
+    }
+        
 }
