63be6bdc884b965b65b57b3e4f3eb1244835c31a
EarthSutiwat <60782337+EarthSutiwat@users.noreply.github.com>
2020-02-08 00:28:58 +0700

    Add files via upload

---
+++ PokemonGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class PokemonGame{
+
+    public static void main(String [] args){
+
+        Pokemon Pichu= new Pokemon();
+        Pokemon Mankey = new Pokemon();
+
+        
+        Pichu.showStatus();
+        
+    }
+}
